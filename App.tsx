4b9374631279fb1b78530e3087e1ac7d5b4c1724
Aayushajs <aayushj004@gmail.com>
2025-11-19 21:47:56 +0530

    fiexd issue

--- App.tsx
+++ App.tsx
@@ -1,67 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
 
-import { StatusBar, StyleSheet, Text, useColorScheme } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent isDarkMode={isDarkMode} />
-    </SafeAreaProvider>
+    <View className="flex-1 bg-purple-600 p-4 py-20">
+  <Text className="text-white text-3xl font-bold">
+    TailwindCSS Working! 🎉
+  </Text>
+</View>
   );
 }
-
-function AppContent({ isDarkMode }: { isDarkMode: boolean }) {
-  return (
-    <SafeAreaView
-      style={[
-        styles.container,
-        isDarkMode ? styles.containerDark : styles.containerLight,
-      ]}
-      edges={['top', 'bottom', 'left', 'right']}
-    >
-      <Text
-        style={[
-          styles.message,
-          isDarkMode ? styles.messageDark : styles.messageLight,
-        ]}
-      >
-        Hello, World!
-      </Text>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerDark: {
-    backgroundColor: '#030712',
-  },
-  containerLight: {
-    backgroundColor: '#F9FAFB',
-  },
-  message: {
-    fontSize: 28,
-    fontWeight: '600',
-  },
-  messageDark: {
-    color: '#F9FAFB',
-  },
-  messageLight: {
-    color: '#030712',
-  },
-});
-
-export default App;
